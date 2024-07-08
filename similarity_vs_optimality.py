12af0f4cae51d46506281f380d1cceaf6cdb9960
Kalina König <Kalina.Koenig@GMX.de>
2024-07-08 22:49:56 +0200

    Update similarity_vs_optimality.py

--- similarity_vs_optimality.py
+++ similarity_vs_optimality.py
@@ -3,18 +3,18 @@ import numpy as np
 from knotenpaare_neu import knotenpaare
 from Graph_Algorithm import prep, adjacency, dijkstra, dijkstra_component, visual
 
-def similarity_optimality:
+def similarity_optimality(graph_times):
   normalization = np.linspace(0, 1, 20)
   values = []
   for t in normalization:
-    k = knotenpaare(1-t)
-    p = prep(k)
+    k = knotenpaare(t)
+    p = prep(graph_times, k)
     a = adjacency(p)
-    t, _ = dijkstra_component(a)
-    values.append(t)
+    time, _ = dijkstra_component(a)
+    values += [time * 60]
 
   plt.plot(normalization, values, marker='o', linestyle='-')
   plt.xlabel('Factor')
-  plt.ylabel('Time taken')
+  plt.ylabel('Time taken in minutes')
   plt.title('How this similarity component effects the optimality of our shortest path')
   plt.show()
